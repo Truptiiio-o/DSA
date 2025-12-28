48758e8ed3f3cd2a0890a93b59d529b43fa30059
Truptiiio-o <tanuaran@gmail.com>
2025-12-28 23:01:49 +0530

    Add problem statement comment for rotated array function
    Added a comment explaining the problem statement for the rotated array function.

--- Arrays/Q3.cpp
+++ Arrays/Q3.cpp
@@ -1,3 +1,18 @@
+/*Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+There may be duplicates in the original array.
+Example 1:
+Input: nums = [3,4,5,1,2]
+Output: true
+Explanation: [1,2,3,4,5] is the original sorted array.
+You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].
+Example 2:
+Input: nums = [2,1,3,4]
+Output: false
+Explanation: There is no sorted array once rotated that can make nums.
+
+*/
+
+
 #include<iostream>
 using namespace std;
 
