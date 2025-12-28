6cced891d748cd851f9e39c6822fcda3650be321
Truptiiio-o <tanuaran@gmail.com>
2025-12-28 23:00:12 +0530

    Create Q4.cpp

---
+++ Arrays/Q4.cpp
@@ -0,0 +1,23 @@
+/*Given an array A[] of size n. The task is to find the largest element in it.
+Example:
+Input:
+n = 5
+A[] = {1, 8, 7, 56, 90}
+Output:
+90
+Explanation:
+The largest element of given array is 90
+*/
+
+#include<iostream>
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int> &nums){
+  vector<vector<int>> ans;
+  sort(nums.begin(),nums.end());
+  for (int i = 0; i < nums.size(); i++){
+    
+  }
+
+  
+}
