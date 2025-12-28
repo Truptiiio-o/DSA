04b843b70ef41ac42959d5003d783d887ed09f07
Truptiiio-o <tanuaran@gmail.com>
2025-12-28 23:02:24 +0530

    Update Q4.cpp

--- Arrays/Q4.cpp
+++ Arrays/Q4.cpp
@@ -1,13 +1,4 @@
-/*Given an array A[] of size n. The task is to find the largest element in it.
-Example:
-Input:
-n = 5
-A[] = {1, 8, 7, 56, 90}
-Output:
-90
-Explanation:
-The largest element of given array is 90
-*/
+
 
 #include<iostream>
 using namespace std;
