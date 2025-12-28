abf6e5b128eb33ece22135470af019272d2da7d6
Truptiiio-o <tanuaran@gmail.com>
2025-12-28 23:01:00 +0530

    Add problem statement for second largest element
    Added a comment block explaining the problem statement for finding the second largest distinct element in an array.

--- Arrays/Q2.cpp
+++ Arrays/Q2.cpp
@@ -1,3 +1,14 @@
+/*Given an array Arr of size N, print the second largest distinct element from an array.
+Example:
+Input:
+N = 6
+Arr[] = {12, 35, 1, 10, 34, 1}
+Output: 34
+Explanation: The largest element of the
+array is 35 and the second largest element
+is 34.
+*/
+
 #include<iostream>
 using namespace std;
 
