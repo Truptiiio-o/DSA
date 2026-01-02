2200ccfc0fa983c377fdb726478602e71296e033
Truptiiio-o <tanuaran@gmail.com>
2026-01-02 19:52:34 +0530

    delete all the occurances of a key in DLL
    to dlete all the occurances of a node in DLL .

--- Arrays/Q7.cpp
+++ Arrays/Q7.cpp
@@ -1,16 +1,27 @@
+/*
+You are given the head of a doubly Linked List and a key. Your task is to delete all occurrences of the given key and return the new doubly Linked List.
+
+**Example:**
+
+Input: 
+2 <-> 2 <-> 10 <-> 8 <-> 4 <-> 2 <-> 5 <-> 2
+Key: 2
+Output: 
+10 <-> 8 <-> 4 <-> 5
+*/
+
+
 ListNode* delete_DLL(ListNode* head, int k){
   ListNode* temp=head;  
   Listnode* back=NULL;
  while(temp!=NULL){ 
   ListNode* front = temp->next;
-    if(temp->val==k){
-      
-      
+/* iffff started*/    if(temp->val==k){
       if(temp==head) {
-      head=front;
-        if(head!=NULL){
-          head->prev=NULL;
-        }
+        head=front;
+          if(front!=NULL){
+            front->prev=NULL;
+          }
       }
       else{      
       back->next=front;
@@ -23,7 +34,7 @@ ListNode* delete_DLL(ListNode* head, int k){
       temp=front;
   
 }
-else{
+/* else of that iffff*/ else{
 
   back=temp;
   temp=front;
