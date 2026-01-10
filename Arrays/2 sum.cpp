a3cac5b48301fe69faf858e7698924528b9fefa6
Truptiiio-o <tanuaran@gmail.com>
2026-01-10 10:42:59 -0800

    Document two sum problem and approach in comments
    Added problem statement and approach for two sum solution.

--- Arrays/2 sum.cpp
+++ Arrays/2 sum.cpp
@@ -1,3 +1,30 @@
+/*
+QUESITON:-
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+*/
+
+/* APPROACH 
+    1. SORT
+    2. I=0 J=N-1
+    3. Check sum== target .. if yes return the original index values of each
+    4. sum<target so we need to increase the value therefore i++
+    5. else sum>target to decrease the value j--
+    6. till i<j
+    */
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
