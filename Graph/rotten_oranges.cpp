b8ae027b88df9d53a6fae44630fcbd120357cc26
Truptiiio-o <tanuaran@gmail.com>
2026-02-20 17:21:43 +0530

    Enhance comments for BFS approach in rotten oranges solution
    Added comments explaining the BFS approach for solving the rotten oranges problem, detailing the algorithm's steps and rationale.

--- Graph/rotten_oranges.cpp
+++ Graph/rotten_oranges.cpp
@@ -1,4 +1,14 @@
+/*
+You are given an m x n grid where each cell can have one of three values:
+0 representing an empty cell,
+1 representing a fresh orange, or
+2 representing a rotten orange.
+Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
 
+Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+Output: 4
+    */
 
 class Solution {
 public:
@@ -50,8 +60,18 @@ public:
                 }
             }
         }
-
         return minutes;
     }
 };
+
+        //BASICALLY WE USED BFS CUZ WE NEED TO CHECK THE ENIGHBOURS OR THAT LEEL EENTS FRIST RATHER THAN GOING TO THE DEPTH 
+        // BOTH WOULD WORK BUT BFS IS BETTER 
+        /*
+APPROACH
+1) since the rotten oranges number will be anyways canged to 2 , so no need f a sperate viisted array 
+2) normal bfs travel but here the chnage is the maintaining a time variable 
+3)we need to increment the time when all the neighbours of a rotten orange are rotten , if no orange got rotten then no increment in time 
+4) so we created a time variable that gets increment only once while its neighbours are getting rotten
+
+            */
 
