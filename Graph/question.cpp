e1487eaebf8a06278a84597f879f07fc46d35108
Truptiiio-o <tanuaran@gmail.com>
2026-02-28 18:31:07 +0530

    Implement cycle detection in graph using BFS
    Added a function to detect cycles in a graph using BFS.

--- Graph/question.cpp
+++ Graph/question.cpp
@@ -1,1 +1,15 @@
-hhhh
+#include<iostream>
+
+/// detect a cycle in a graph using BFS 
+bool detect(int src,vector<int> adj[],int vis[]){
+  vis[src]=1;
+  queue<pair<int,int>> q;
+  q.push({src,-1});
+  while(!q.empty()){
+    int node=q.front().first;
+    int parent=q.front().second;
+    
+  }
+}
+
+
