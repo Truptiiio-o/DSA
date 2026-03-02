86b210cd9981d549bb7a24be22d1a4a7de355ad3
Truptiiio-o <tanuaran@gmail.com>
2026-03-02 11:57:55 +0530

    Update to detect a cycleBFS.cpp

--- Graph/detect a cycleBFS.cpp
+++ Graph/detect a cycleBFS.cpp
@@ -8,8 +8,26 @@ bool detect(int src,vector<int> adj[],int vis[]){
   while(!q.empty()){
     int node=q.front().first;
     int parent=q.front().second;
-    
+    q.pop();
+    for(auto it:adj[node]){
+      if(!vis[it]){
+        vis[it]=1;
+        q.push({it,node});
+      }
+      else if(parent!=it){
+        return true;
+      }
+    }
   }
+  return false;
 }
 
 
+int main(){
+
+ // smthg smtg
+}
+
+
+//TC=O(N+2E)
+  //sc=O(N)
