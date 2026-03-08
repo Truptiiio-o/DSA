efb49224dd619ab1ba07cd79f957a89b85ddcd3c
Truptiiio-o <tanuaran@gmail.com>
2026-03-08 15:47:39 +0530

    Add Dijkstra's algorithm implementation
    Implement Dijkstra's algorithm for shortest paths.

---
+++ Graph/dijkstar2.cpp
@@ -0,0 +1,25 @@
+vector<int> dijkstra(int V, vector<vector<int>> adj[] ,int S){
+  set<pair<int,int>> st;
+  vector<int> dist(V,1e9);
+  st.insert({0,S});
+  dist[S]=0;
+  while(!st.empty()){
+    auto it=*(st.begin());
+    int node=it.second;
+    int dis=it.first;
+    st.erase(it);
+    for( auto it:adj[node]){
+      int adjnode=it[0];
+      int weight=it[1];
+      if(dis+weight<dist[adjnode]){
+        if(dist[adjnode]!=1e9) st.erase({dist[adjnode],adjnode}];
+
+        dist[adjnode]=weight+dis;
+        st.insert({dist[adjnode],adjnode});
+      }
+    }
+    
+  }
+  return dist
+  
+}
