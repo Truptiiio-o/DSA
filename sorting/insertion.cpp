2e517cff1b44281f1f3264f3d5166f1449233de8
Truptiiio-o <tanuaran@gmail.com>
2026-06-12 16:58:38 +0530

    Implement insertion sort algorithm

---
+++ sorting/insertion.cpp
@@ -0,0 +1,10 @@
+void insertion(vector<int> &arr){
+  int n=arr.size();
+  for(int i=1;i<n;i++){
+    int j=i;
+    while(j>0 && arr[j]<arr[j-1]){
+      swap(arr[j],arr[j-1]);
+      j--;
+    }
+  }
+}
