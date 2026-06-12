651ef026cb893a8870eeb4c770afcffcb5c78c05
Truptiiio-o <tanuaran@gmail.com>
2026-06-12 16:45:03 +0530

    Add selection sort function
    Implement selection sort algorithm for sorting an array.

---
+++ sorting/selection_sort.cpp
@@ -0,0 +1,12 @@
+void selection(vector<int> &arr){
+  int n=arr.size();
+  for(int i=0;i<n-1;i++){
+    int mini=i;
+    for(int j=i;j<n;j++){
+      if(arr[j]<arr[mini]){
+        mini=j;
+      }
+      swap(arr[i],arr[mini]);
+      }
+  }  
+}
